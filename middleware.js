3710f0fec8de2b2e0ad0d3b4b40d509fed15ad68
chrisgilbert722 <chrisgilbert722@gmail.com>
2025-11-15 08:57:01 -0500

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,23 +1,5 @@
 import { NextResponse } from "next/server";
 
 export function middleware(req) {
-  const url = req.nextUrl.clone();
-  const token = req.cookies.get("sb-access-token")?.value;
-
-  const protectedRoutes = [
-    "/dashboard",
-    "/vendors",
-    "/upload-coi",
-    "/vendor-upload",
-    "/api",
-  ];
-
-  if (protectedRoutes.some((r) => url.pathname.startsWith(r))) {
-    if (!token) {
-      url.pathname = "/auth/login";
-      return NextResponse.redirect(url);
-    }
-  }
-
   return NextResponse.next();
 }
