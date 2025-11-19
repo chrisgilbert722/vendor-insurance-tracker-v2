c461cfa6ecbdc73e610340a3e7947caed10b724e
chrisgilbert722 <chrisgilbert722@gmail.com>
2025-11-19 08:27:03 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,13 @@
 // pages/_app.js
 import { OrgProvider } from "../context/OrgContext";
 import Layout from "../components/Layout";
-import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
-import { SessionContextProvider } from "@supabase/auth-helpers-react";
-import { useState } from "react";
 
 export default function App({ Component, pageProps }) {
-  const [supabaseClient] = useState(() =>
-    createBrowserSupabaseClient()
-  );
-
   return (
-    <SessionContextProvider
-      supabaseClient={supabaseClient}
-      initialSession={pageProps.initialSession}
-    >
-      <OrgProvider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </OrgProvider>
-    </SessionContextProvider>
+    <OrgProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </OrgProvider>
   );
 }
