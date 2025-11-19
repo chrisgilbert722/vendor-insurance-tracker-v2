bbd3217b27d3e50e837ee7df1eaeeb1a94695957
chrisgilbert722 <chrisgilbert722@gmail.com>
2025-11-19 16:36:14 -0500

    Update index.js

--- components/charts/index.js
+++ components/charts/index.js
@@ -1,3 +1,4 @@
 // components/charts/index.js
 export { default as ComplianceTrajectoryChart } from "./ComplianceTrajectoryChart";
 export { default as PassFailDonutChart } from "./PassFailDonutChart";
+export { default as ExpiringCertsHeatmap } from "./ExpiringCertsHeatmap"; // 🔥 NEW
