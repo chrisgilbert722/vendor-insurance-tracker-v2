5b94bdda8123a9d68370d70b4a5636f3e3db375c
chrisgilbert722 <chrisgilbert722@gmail.com>
2025-12-14 17:16:50 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,14 +4,6 @@ import { UserProvider } from "../context/UserContext";
 import { OrgProvider } from "../context/OrgContext";
 import Layout from "../components/Layout";
 
-/**
- * SAFE BOOT MODE
- * - No redirects
- * - No onboarding enforcement
- * - No AdminGuard
- * - No conditional hooks
- * Goal: app must load again, always.
- */
 export default function App({ Component, pageProps }) {
   return (
     <UserProvider>
