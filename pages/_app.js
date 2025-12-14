2b7f62ff42678bf64ada2396c01991acac20f003
chrisgilbert722 <chrisgilbert722@gmail.com>
2025-12-14 17:44:15 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,17 @@
 // pages/_app.js
 import "../public/cockpit.css";
 import { UserProvider } from "../context/UserContext";
+import { OrgProvider } from "../context/OrgContext";
+import Layout from "../components/Layout";
 
 export default function App({ Component, pageProps }) {
   return (
     <UserProvider>
-      <Component {...pageProps} />
+      <OrgProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </OrgProvider>
     </UserProvider>
   );
 }
