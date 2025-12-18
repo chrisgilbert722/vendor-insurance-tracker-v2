96bfa4f17613241746d0746bbd3e54ab05015c75
chrisgilbert722 <chrisgilbert722@gmail.com>
2025-12-18 15:40:58 -0500

    Update get.js

--- pages/api/admin/sso/get.js
+++ pages/api/admin/sso/get.js
@@ -21,7 +21,6 @@ export default async function handler(req, res) {
       SELECT
         id,
         name,
-        domain,
         allowed_domains,
         external_uuid,
         sso_provider,
