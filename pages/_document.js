65fe4bd7a81f7deccf67233176ff832f2575d55a
chrisgilbert722 <chrisgilbert722@gmail.com>
2025-12-25 16:10:12 -0500

    Create _document.js
    Add global document with Verivo favicon

---
+++ pages/_document.js
@@ -0,0 +1,27 @@
+// pages/_document.js
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/brand/favicons/favicon-32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="48x48"
+          href="/brand/favicons/favicon-48.png"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
