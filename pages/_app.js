2e67a4c98ae9599c0b3b732f1c90a78c339e524d
chrisgilbert722 <chrisgilbert722@gmail.com>
2025-12-28 15:00:09 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,34 @@
 // pages/_app.js
 import "../public/cockpit.css";
+import Script from "next/script";
 import { UserProvider } from "../context/UserContext";
 import { OrgProvider } from "../context/OrgContext";
 import Layout from "../components/Layout";
 
 export default function App({ Component, pageProps }) {
   return (
-    <UserProvider>
-      <OrgProvider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </OrgProvider>
-    </UserProvider>
+    <>
+      {/* Google Analytics (GA4) */}
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-M5YME3TEQ1"
+      />
+      <Script id="ga4-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-M5YME3TEQ1');
+        `}
+      </Script>
+
+      <UserProvider>
+        <OrgProvider>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </OrgProvider>
+      </UserProvider>
+    </>
   );
 }
