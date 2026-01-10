89e7dd74f0453682e9bb0977b231d7ca1939f491
chrisgilbert722 <chrisgilbert722@gmail.com>
2026-01-10 08:28:19 -0500

    Update update-emails.js

--- pages/api/vendors/update-emails.js
+++ pages/api/vendors/update-emails.js
@@ -96,8 +96,7 @@ export default async function handler(req, res) {
 
       const result = await sql`
         UPDATE vendors
-        SET email = ${v.email},
-            updated_at = now()
+        SET email = ${v.email}
         WHERE org_id = ${orgIdInt}
           AND name = ${v.vendorName};
       `;
