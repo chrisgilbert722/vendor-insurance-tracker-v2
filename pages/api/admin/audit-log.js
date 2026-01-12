08339e9f2e5bf302716c2d0c0097f01424c82b5f
chrisgilbert722 <chrisgilbert722@gmail.com>
2026-01-12 16:59:27 -0500

    Update audit-log.js

--- pages/api/admin/audit-log.js
+++ pages/api/admin/audit-log.js
@@ -1,5 +1,5 @@
-import { sql } from "../../../lib/db";
-import { resolveOrg } from "../../../lib/resolveOrg";
+import { sql } from "@db";
+import { resolveOrg } from "@resolveOrg";
 
 export default async function handler(req, res) {
   try {
