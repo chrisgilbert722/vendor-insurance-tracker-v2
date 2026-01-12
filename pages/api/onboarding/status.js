35baf2c63873136d6dd926ee50c6ab78ee26ab61
chrisgilbert722 <chrisgilbert722@gmail.com>
2026-01-12 17:39:01 -0500

    Update status.js

--- pages/api/onboarding/status.js
+++ pages/api/onboarding/status.js
@@ -7,8 +7,8 @@
 // - Backend is single source of truth
 // ============================================================
 
-import { sql } from "../../../lib/db";
-import { resolveOrg } from "../../../lib/resolveOrg";
+import { sql } from "@db";
+import { resolveOrg } from "@resolveOrg";
 
 export default async function handler(req, res) {
   if (req.method !== "GET") {
